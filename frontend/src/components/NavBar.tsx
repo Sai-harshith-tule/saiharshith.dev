1701d82305468977ab0d3c8586d92c50c6865732
Sai Harshith Tule <saiharshith061203@gmail.com>
2026-09-13 20:05:52 +0530

    Add NavBar.tsx component

---
+++ frontend/src/components/NavBar.tsx
@@ -0,0 +1,92 @@
+// import React from 'react'
+import {useState} from "react";
+import {FaCode, FaEnvelope, FaHome, FaProjectDiagram, FaUser} from 'react-icons/fa'
+import {motion} from 'framer-motion'
+import {Moon, Sun} from "lucide-react";
+
+// @ts-ignore TODO: remove this after enforcing the types
+const NavBar = ({darkMode, toggleDarkMode}) => {
+    //HOOKS
+    const[activeTab, setActiveTab] = useState('Home')
+
+    //Navigation Bar Items
+    //TODO: Update 'EXPERIENCE' icon with a proper one from react-icons/fa
+    const navItems = [
+        {name: 'Home', link: '#home', icon: FaHome},
+        {name: 'About', link: '#about', icon: FaUser},
+        {name: 'Experience', link: '#experience', icon: FaHome},
+        {name: 'Projects', link: '#projects', icon: FaProjectDiagram},
+        {name: 'Skills', link: '#skills', icon: FaCode},
+        {name: 'Contact', link: '#contact', icon: FaEnvelope}
+    ]
+    return (
+        <div className='fixed z-50 bottom-0 left-0 right-0 flex justify-center'>
+            <motion.nav
+                initial={{ y: 100 }}
+                animate={{ y: 0 }}
+                transition={{duration: 0.5}}
+                className='relative w-[95%] max-w-4xl mb-4'
+            >
+                {/* Dark & light mode toggle button */}
+                <div className='relative bg-linear-to-r from-red-600 to-red-800 backdrop-blur-xl rounded-2xl shadow-2xl
+                 border border-white/20 px-3 py-2'>
+                    <div className='absolute -top-5 right-3'>
+                        <motion.button
+                            whileHover={{ scale: 1.1 }}
+                            whileTap={{ scale:0.9 }}
+                            onClick={toggleDarkMode}
+                            className='p-2 rounded-full bg-gray-900 dark:bg-gray-100 transition-colors backdrop-blur-sm'
+                        >
+                            {darkMode ? (
+                                <Sun className='w-4 lg:w-5 h-4 lg:h-5 text-black'/>
+                            ) : (
+                                <Moon className='w-5 h-5 text-white'/>
+                            )}
+                        </motion.button>
+                    </div>
+                    {/* Navigation bar Items */}
+                    <div className='flex items-center justify-around gap-1'>
+                        {navItems.map((item) => {
+                            const Icon = item.icon
+                            const isActive = activeTab === item.name
+                            return (
+                                <motion.a
+                                    key={item.name}
+                                    href={item.link}
+                                    onClick={() => setActiveTab(item.name)}
+                                    whileHover={{ scale:1.1 }}
+                                    whileTap={{ scale:0.9 }}
+                                    className='flex flex-col items-center gap-0.5 py-1.5 px-2 relative group flex-1'
+                                >
+                                    {isActive && (
+                                        <motion.div
+                                            layoutId='activeTab'
+                                            className='absolute -top-2 left-1/2 -translate-x-1/2 w-6 h-1 bg-white rounded-full'
+                                            transition={{ duration: 0.3}}
+                                        />)
+                                    }
+                                    {/* Render Icon */}
+                                    <Icon
+                                        className={`w-5 h-5 transition-all duration-300
+                                        ${ isActive ? 'text-white' : 'text-white/70 group-hover:text-white' 
+                                        }`}
+                                    />
+                                    {/* Render Icon name */}
+                                    <span className={`text-[10px] font-medium transition-all duration-300
+                                    ${isActive?'text-white':'text-white/60 group-hover:text-white'}`}
+                                    >
+                                        {item.name}
+                                    </span>
+                                </motion.a>
+                            )
+                        })}
+                    </div>
+
+                </div>
+
+            </motion.nav>
+        </div>
+    )
+}
+
+export default NavBar
