22c9277ed58cad1d898df0b0ebc401d4c2d75625
Sai Harshith Tule <saiharshith061203@gmail.com>
2026-09-14 20:20:31 +0530

    Add emoji component

---
+++ frontend/src/components/Emoji.tsx
@@ -0,0 +1,10 @@
+//import react when needed
+
+//TODO: complete emoji component
+const Emoji = () => {
+    return (
+        <div>Hello this is Emoji component</div>
+    )
+}
+
+export default Emoji
